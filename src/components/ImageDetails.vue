cf721d50d17a1d651211ba7da9ddeb67adce1673
Usairim Isani <usairim.isani@outlook.com>
2020-08-16 23:26:56 +0500

    Feature : Rudimentary Impl of Image Detail View.

--- src/components/ImageDetails.vue
+++ src/components/ImageDetails.vue
@@ -2,15 +2,37 @@
   <!-- Image view -->
   <!-- Views Image of Specific ID -->
   <!-- For All images use Gallery -->
-  <div></div>
+  <div>
+    <img :src="url()" class="default-img-size" />
+  </div>
 </template>
 <script>
+import { mapActions, mapState } from "vuex";
+import ACTION_CONSTANTS from "../store/ACTION_CONSTANTS";
 export default {
   data() {
     return {};
   },
   mounted() {
-    // * this.$route.params.id;
+    // * Get Image
+    this[ACTION_CONSTANTS.GET_IMAGE](this.$route.params.id);
   },
+  methods: {
+    ...mapActions([ACTION_CONSTANTS.GET_IMAGE]),
+    url() {
+      if (this.image.blob) {
+        return URL.createObjectURL(this.image.blob);
+      } else {
+        return "../assets/no-image.svg";
+      }
+    },
+  },
+  computed: mapState({
+    image: (state) => {
+      return {
+        ...state.Gallery.image,
+      };
+    },
+  }),
 };
 </script>
